b84f51ee048662bc7cf1b674be86c8dc94c9a041
exec <66413427+execuu@users.noreply.github.com>
2025-06-25 12:20:54 +0800

    Update run_app.py

--- run_app.py
+++ run_app.py
@@ -2,9 +2,7 @@ import os
 import subprocess
 import webbrowser
 
-# Get full path to main.py
 app_path = os.path.join(os.path.dirname(__file__), '/home/execu/Programming/CalCS/jb/main.py')
 
-# Launch Streamlit with the full path
 subprocess.Popen(f"streamlit run \"{app_path}\"", shell=True)
 webbrowser.open("http://localhost:8501")
